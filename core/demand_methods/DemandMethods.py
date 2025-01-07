c9971298718d8485db8ddb4d44f63deb78e4e320
robert-wienroeder <kontakt@robert-wienroeder.de>
2025-01-07 10:58:12 +0100

    added docstrings, changed variable names

--- core/demand_methods/DemandMethods.py
+++ core/demand_methods/DemandMethods.py
@@ -3,12 +3,22 @@
 
 
 # Robert's demand function
-def robert_demands(dataframe_column_1, dataframe_column_2):
-    '''
-    Takes the charging station vector and the population vector and returns the demand based on the formula we devised
-    Input: 
-        - dataframe_column_1: 
-        - dataframe_column_2: 
-    Output: vector of charging station demand for each PLZ
-    '''
-    return (0.01 * dataframe_column_2 / 10).sub(dataframe_column_1).astype(int)
+def robert_demands(gdf_residents_preprocessed, gdf_charging_station_counts):
+    """
+    Calculates Charging Station Demand per Postal Code
+    Inputs: gdf_residents_preprocessed - residents vector; gdf_charging_station_counts - charging station counts vector
+    Outputs: Vector of charging station demand as integers
+    Postconditions: Demand is calculated using the formula (0.01 * station counts / 10) - residents
+
+    Formula: (EV * P / EVPCS) - CS
+    with:
+    EV = Electric Vehicles per resident (currently 0.01)
+    P = Population per postal code
+    EVPCS = Electric Vehicles per Charging Station (recommended are 10)
+    CS = Charging Stations that already exist in the postal code area
+
+    """
+
+    return (0.01 * gdf_residents_preprocessed / 10).sub(gdf_charging_station_counts).astype(int)
+
+
