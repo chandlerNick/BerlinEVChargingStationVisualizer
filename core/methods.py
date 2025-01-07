bca7c371243c6975995ad1adf0f8139106c2a7d1
Nicholas Chandler <chandln@wwu.edu>
2025-01-07 16:30:36 +0100

    Adding Functionality

--- core/methods.py
+++ core/methods.py
@@ -255,7 +255,7 @@ def make_streamlit_electric_Charging_resid(df_charging_stations, df_population):
         # Button to submit the suggestion
         if st.button("Submit Suggestion"):
             if suggestion.strip() and postal_code.strip():  # Check if the suggestion is not empty
-                if postal_code.strip() in df_charging_stations_copy['PLZ']:
+                if int(postal_code.strip()) in df_charging_stations_copy['PLZ']:
                     st.session_state["suggestions"].append({
                         "Text": suggestion.strip(),
                         "PLZ": postal_code.strip()})
